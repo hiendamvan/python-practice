f27661a1d359963c5ad3275fd3dcc8d3d5ad230e
hienvandam <22022664@vnu.end.vn>
2024-01-30 11:19:24 +0700

    FIx linting 1. Add docstring 2. Snake_case 3. Black lines

--- number_of_ways.py
+++ number_of_ways.py
@@ -1,16 +1,15 @@
+"""Solve leetcode problem"""
 from tqdm import tqdm
 
 
 def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     """
     Solving Leetcode Problem.
-    https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
-    
+    https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps
     Given two positive integers startPos and endPos
     Initially, you are standing at position startPos on an infinite
     number line. With one step, you can move either one position to the left,
-    or one position to the right.
-    
+    or one position to the right
     Given a positive integer k, return the number of different ways to
     reach the position endPos starting from startPos, such that you
     perform exactly k steps.
@@ -58,6 +57,7 @@ def test_number_of_ways():
     It can be proven that no other way is possible, so we return 3.
     """
     print(numberOfWays(1, 2, 3))
+
 
 if __name__ == "__main__":
     test_number_of_ways()
